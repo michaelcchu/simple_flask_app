6a1e09a76620b23ea2eda527ca32c3099299938a
mccode187 <50280000+mccode187@users.noreply.github.com>
2020-08-09 17:49:12 -0400

    Added tab for simulation features

---
+++ templates/scripts.js
@@ -0,0 +1,8 @@
+function openTab(tabID) {
+  var i, tabcontent;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  document.getElementById(tabID).style.display = "block";
+}
