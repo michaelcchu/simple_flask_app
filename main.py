cd4c48d64ac93d60d369f8b1c4bce5539136b1c1
mccode187 <50280000+mccode187@users.noreply.github.com>
2020-08-23 21:09:43 -0400

    Created the four tabs of the website and the homepage and linked them together

--- main.py
+++ main.py
@@ -7,16 +7,32 @@ import money
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET','POST'])
-def index():
-    displayImage = False
+@app.route('/mpc_m', methods=['GET','POST'])
+def mpc_m():
     condensedMoney = []
     condensedProbs = []
     filename = ''
     deck = {}
+    displayImage = False
 
     if request.method == 'POST':
         deck = json.loads(request.form['deck'].replace("'","\""))
         [condensedMoney,condensedProbs,filename] = money.main(deck,app)
         displayImage = True
-    return render_template('index.html', filename=filename, deck=deck, money=condensedMoney, probs=condensedProbs, displayImage=displayImage)
+    return render_template('mpc_m.html', filename=filename, deck=deck, money=condensedMoney, probs=condensedProbs, displayImage=displayImage)
+
+@app.route('/mpc_s')
+def mpc_s():
+    return render_template('mpc_s.html')
+
+@app.route('/hpc_m')
+def hpc_m():
+    return render_template('hpc_m.html')
+
+@app.route('/sc_s')
+def sc_s():
+    return render_template('sc_s.html')
+
+@app.route('/')
+def home():
+    return render_template('home.html')
